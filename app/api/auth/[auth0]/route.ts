405779c46be05490fd72dd2b187e979ba69d09df
“SantiagoPerez“ <saantiiago50@gmail.com>
2026-05-08 15:29:34 -0300

    Fix/Update Version 1.1v

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,11 +1,12 @@
 import { handleAuth, handleCallback } from '@auth0/nextjs-auth0'
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { createAdminClient } from '@/lib/supabase/server'
 
 export const GET = handleAuth({
-  async callback(req: NextApiRequest, ctx: NextApiResponse) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async callback(req: any, ctx: any) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const res = await handleCallback(req, ctx, {
-      afterCallback: async (_req, session) => {
+      afterCallback: async (_req: any, session: any) => {
         const supabase = createAdminClient()
         const { user } = session
 
